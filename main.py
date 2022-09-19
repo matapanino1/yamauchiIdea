626029635db7c3bc7b7c066ef4ddfac75bccf13c
matapanino <pghby87971@yahoo.co.jp>
2022-09-19 11:05:38 +0900

    2nd commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def randstr(length):
     return ''.join([chr(random.randint(12354, 12435)) for _ in range(length)])
 
 
-st.title('３文字ランダム生成')
+st.title('単語ランダム生成')
 
 number = st.number_input(label='何文字の単語を生成しますか？', value=3)
 
